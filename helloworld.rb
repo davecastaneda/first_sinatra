19fef61f7cb2306f967fa0202ff11b83ff6ce9b5
davecastaneda <smopo24@gmail.com>
2018-07-15 00:43:14 -0500

    First commit

---
+++ helloworld.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+get '/' do
+    "Hello world!"
+end
+
+get '/goodbye' do
+    "Goodbye world!"
+end
+
+get '/about' do
+    output = "<h2>Sinatra</h2>"
+    output  << "<p>This is my first Sinatra app.</p>"
+    output
+end
